3376f88bf002476e3b72c1df842e4ed89fa05826
Reinaldo Augusto Bocchi <reinaldo.bocchi@gmail.com>
2024-05-13 21:05:49 -0300

    updage configmap and add auth middleware

--- src/external/api/v1/middlewares/auth.go
+++ src/external/api/v1/middlewares/auth.go
@@ -5,6 +5,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/hcsouza/fiap-tech-fast-food/src/external/api/infra/config"
 	cognitoJwtVerify "github.com/jhosan7/cognito-jwt-verify"
+	"net/http"
+	"strings"
 )
 
 func CheckAccessToken(allowedRoutes ...string) gin.HandlerFunc {
@@ -14,28 +16,28 @@ func CheckAccessToken(allowedRoutes ...string) gin.HandlerFunc {
 	}
 
 	return func(c *gin.Context) {
-		//if allowed[c.Request.URL.Path] {
-		//	c.Next()
-		//	return
-		//}
-		//
-		//authorizationHeader := c.GetHeader("Authorization")
-		//if authorizationHeader == "" {
-		//	c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
-		//	return
-		//}
-		//
-		//tokenParts := strings.Split(authorizationHeader, " ")
-		//if len(tokenParts) != 2 || strings.ToLower(tokenParts[0]) != "bearer" {
-		//	c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Malformed token"})
-		//	return
-		//}
-		//
-		//accessToken := tokenParts[1]
-		//if !validateAccessToken(accessToken) {
-		//	c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid access token"})
-		//	return
-		//}
+		if allowed[c.Request.URL.Path] {
+			c.Next()
+			return
+		}
+
+		authorizationHeader := c.GetHeader("Authorization")
+		if authorizationHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
+			return
+		}
+
+		tokenParts := strings.Split(authorizationHeader, " ")
+		if len(tokenParts) != 2 || strings.ToLower(tokenParts[0]) != "bearer" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Malformed token"})
+			return
+		}
+
+		accessToken := tokenParts[1]
+		if !validateAccessToken(accessToken) {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid access token"})
+			return
+		}
 		c.Next()
 	}
 }
